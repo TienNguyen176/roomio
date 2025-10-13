f4acb067844ca7e6de113f067a3437167eb45705
Tien-Van-176 <furinawibu176@gmail.com>
2025-10-13 09:09:18 +0700

    Initial project setup

--- app/src/main/java/com/tdc/nhom6/roomio/activities/MainActivity.kt
+++ app/src/main/java/com/tdc/nhom6/roomio/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.tdc.nhom6.roomio
+package com.tdc.nhom6.roomio.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.tdc.nhom6.roomio.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
